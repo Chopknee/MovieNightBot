af9d810d11e3b268de2552bdcd071b6098dd405e
fdsf <Chopk@DEfdsfSKTOP-DFVIUS0>
2019-04-14 20:21:25 -0400

    Fixed a bug where users that block this bot can't get help messaged and don't have any feedback.

--- MovieNightBot/Core/Commands/Help.cs
+++ MovieNightBot/Core/Commands/Help.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Discord.Net;
 
 namespace MovieNightBot.Core.Commands {
     public class Help : ModuleBase<SocketCommandContext> {
@@ -14,35 +15,42 @@ namespace MovieNightBot.Core.Commands {
 
         [Command("help"), Summary("Get sum help")]
         public async Task GetHelp() {//Because we could all use some from time to time.
-            EmbedBuilder builder = new EmbedBuilder()
-                .WithTitle("MovieNightBot Help")
-                .WithDescription("I heard you asked for some help. We all need some from time to time, so here it is.\n" +
-                "Commands marked with :no_entry: require the user to have the role \"Movie Master\", unless the server owner has changed it.\n" +
-                "Valid command prefixes include m! M! or you may @ movie night bot.")
-                .WithColor(new Color(0xFFFFFF))
-                .WithTimestamp(DateTime.Now)
-                .WithAuthor(author => {
-                    author
-                    .WithName("Movie Night Bot");
-                });
-            builder.AddField("m!help", "DMs the sender of the command this exact message. Now that's meta!");
-            builder.AddField("m!suggest [Title]", "Adds the supplied movie to the suggestions list. There is a chance this movie will now show up on future votes.");
-            builder.AddField("m!watched", "Lists all movies that have been watched.");
-            builder.AddField("m!suggested", "Lists all movies that have been suggested.");
-            builder.AddField("m!set_watched [Title]", "Sets the specified movie as having been watched. This movie will not show up on future votes.");
-            builder.AddField("m!unwatch [Title]", "Removes the specified movie from the watched list.");
-            builder.AddField("m!remove [Title]", ":no_entry:  Removes the specified movie from the suggestions list.");
-            builder.AddField("m!start_vote", "Selects a number of random movie suggestions to be voted on.");
-            builder.AddField("m!end_vote", $"Ends the currently running vote and displays the winning vote. Reacting to the vote embed with {MojiCommand.CommandMoji[0]} will also end the vote.");
-            builder.AddField("m!user_vote_count [Number]", ":no_entry: Sets the number of movies users will be allowed to vote on. This cannot be greater than the number of movie options that shows on a vote.");
-            builder.AddField("m!movie_option_count [Number]", ":no_entry:  Sets the number of movies that will show up on a vote.");
-            builder.AddField("m!tie_option [option]", ":no_entry:  Sets how the bot handles tied votes.\n Option **breaker** will make a new vote using only the tied movies.\n" +
-                "Option **random** will make a new vote with a random selection of movies.");
-            builder.AddField("m!set_admin_role [Name]", ":no_entry: Sets the name of the role that is allowed to run admin only commands in movie night bot. WARNING, you will not be able to run commands if you have no users with this name!!!");
-            builder.AddField("m!get_admin_role [Name]", "Gets the name of the role that is allowed to run admin only commands for Movie Night Bot. Use for emergencies.");
-            builder.AddField("m!set_movie_time [Number]", ":no_entry: Sets the hour when the movie will be watched. The hour is in UTC time zone, so convert accordingly. Valid range is 0 - 23 This time shows at the bottom of the vote embed.");
-            Embed embed = builder.Build();
-            await Context.User.SendMessageAsync("You Need Some Help", embed: embed).ConfigureAwait(false);
+            try {
+                EmbedBuilder builder = new EmbedBuilder()
+                    .WithTitle("MovieNightBot Help")
+                    .WithDescription("I heard you asked for some help. We all need some from time to time, so here it is.\n" +
+                    "Commands marked with :no_entry: require the user to have the role \"Movie Master\", unless the server owner has changed it.\n" +
+                    "Valid command prefixes include m! M! or you may @ movie night bot.")
+                    .WithColor(new Color(0xFFFFFF))
+                    .WithTimestamp(DateTime.Now)
+                    .WithAuthor(author => {
+                        author
+                        .WithName("Movie Night Bot");
+                    });
+                builder.AddField("m!help", "DMs the sender of the command this exact message. Now that's meta!");
+                builder.AddField("m!suggest [Title]", "Adds the supplied movie to the suggestions list. There is a chance this movie will now show up on future votes.");
+                builder.AddField("m!watched", "Lists all movies that have been watched.");
+                builder.AddField("m!suggested", "Lists all movies that have been suggested.");
+                builder.AddField("m!set_watched [Title]", "Sets the specified movie as having been watched. This movie will not show up on future votes.");
+                builder.AddField("m!unwatch [Title]", "Removes the specified movie from the watched list.");
+                builder.AddField("m!remove [Title]", ":no_entry:  Removes the specified movie from the suggestions list.");
+                builder.AddField("m!start_vote", "Selects a number of random movie suggestions to be voted on.");
+                builder.AddField("m!end_vote", $"Ends the currently running vote and displays the winning vote. Reacting to the vote embed with {MojiCommand.CommandMoji[0]} will also end the vote.");
+                builder.AddField("m!user_vote_count [Number]", ":no_entry: Sets the number of movies users will be allowed to vote on. This cannot be greater than the number of movie options that shows on a vote.");
+                builder.AddField("m!movie_option_count [Number]", ":no_entry:  Sets the number of movies that will show up on a vote.");
+                builder.AddField("m!tie_option [option]", ":no_entry:  Sets how the bot handles tied votes.\n Option **breaker** will make a new vote using only the tied movies.\n" +
+                    "Option **random** will make a new vote with a random selection of movies.");
+                builder.AddField("m!set_admin_role [Name]", ":no_entry: Sets the name of the role that is allowed to run admin only commands in movie night bot. WARNING, you will not be able to run commands if you have no users with this name!!!");
+                builder.AddField("m!get_admin_role [Name]", "Gets the name of the role that is allowed to run admin only commands for Movie Night Bot. Use for emergencies.");
+                builder.AddField("m!set_movie_time [Number]", ":no_entry: Sets the hour when the movie will be watched. The hour is in UTC time zone, so convert accordingly. Valid range is 0 - 23 This time shows at the bottom of the vote embed.");
+                Embed embed = builder.Build();
+                await Context.User.SendMessageAsync("You Need Some Help", embed: embed).ConfigureAwait(false);
+            } catch (HttpException ex) {
+                await Context.Channel.SendMessageAsync("I can't send you the help message right now, you might have me blocked or something.");
+            } catch (Exception ex) {
+                await Program.Instance.Log(new LogMessage(LogSeverity.Error, "Server Settings", "A general exception was raised.", ex));
+                await Context.Channel.SendMessageAsync("I'm not really sure what happened but something went wrong while executing that command, sorry. :flushed:");
+            }
         }
     }
 }
